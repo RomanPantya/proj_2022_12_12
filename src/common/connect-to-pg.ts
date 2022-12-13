ea22863fcbee9f1f09fb88652720f46b03f92b94
RomanPantya <romanpantya@gmail.com>
2022-12-13 06:02:45 +0200

    add connect-to-pg.ts

--- src/common/connect-to-pg.ts
+++ src/common/connect-to-pg.ts
@@ -1,1 +1,22 @@
 import { Pool } from 'pg';
+import { config } from 'dotenv';
+import { join } from 'path';
+
+config({ path: join(process.cwd(), '.env') });
+config({ path: join(process.cwd(), '.default.env') });
+
+export async function connectToPg() {
+    const pool = new Pool({
+        user: process.env.PG_USER,
+        password: process.env.PG_PASS,
+        host: process.env.PG_HOST,
+        database: process.env.PG_BASE,
+        port: +process.env.PG_PORT,
+    });
+
+    const connection = pool.connect();
+
+    console.info('Connect to Postgres');
+
+    return connection;
+}
